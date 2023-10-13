64b1bb2633c8b3f16dea1084490cca6a6a0ce08e
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-10-13 13:51:03 -0300

    menu links

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+let body = document.querySelector('body');
 let menu_icon = document.querySelector('header h1 i');
 let menu_is_open = false;
 let header = document.querySelector('header');
@@ -10,3 +11,21 @@ menu_icon.addEventListener('click', () => {
   menu_icon.className = menu_is_open ? 'fa-solid fa-x' : 'fa-solid fa-bars';
 });
 
+
+function scroll() {
+  if (window.scrollY > 200) {
+    header.style.background = 'rgba(10, 0, 25, 0.75)';
+  } else {
+    header.style.background = 'none';
+  } 
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
